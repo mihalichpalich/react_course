ff206248b38dc03fee2cf59f116d6520df97d87d
mihalichpalich <pavlovtfspb@gmail.com>
2018-01-31 12:24:20 +0300

    изучил свойства

--- app.jsx
+++ app.jsx
@@ -1,11 +1,20 @@
-function Hero() {
+function Hero(props) { //получаем объект со свойствами из рендера
   return (
     <div className="container">
-      <img src="https://cdn.worldvectorlogo.com/logos/react.svg" />
-      <h1>React</h1>
-      <p>Библиотека для создания пользовательских интерфейсов</p>
+      <img src={props.imageUrl} />
+      <h1>{props.title}</h1>
+      <p>{props.subtitle}</p>
     </div>
   );
 }
 
-ReactDOM.render(<Hero />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <Hero title="React"
+        subtitle="Библиотека для создания пользовательских интерфейсов"
+        imageUrl="https://cdn.worldvectorlogo.com/logos/react.svg" />
+    <Hero title="Angular 2"
+        subtitle="Один фремворк"
+        imageUrl="https://angular.io/assets/images/logos/angular/angular.png" />
+  </div>,
+document.getElementById('root')); //2 компонета нельзя передавать одновременно, их нужно обернуть
