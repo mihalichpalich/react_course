97c824f4196c240c0c83075f925adf015b1b81de
mihalichpalich <pavlovtfspb@gmail.com>
2018-01-31 13:28:16 +0300

    изучил состояния

--- app.jsx
+++ app.jsx
@@ -1,12 +1,43 @@
-function Hero(props) { //получаем объект со свойствами из рендера
-  return (
-    <div className="container">
-      <img src={props.imageUrl} />
-      <h1>{props.title}</h1>
-      <p>{props.subtitle}</p>
-    </div>
-  );
-}
+// function Hero(props) { //получаем объект со свойствами из рендера
+//   let count = 0; //count нельзя получить как свойство, потому что свойства не меняются
+
+//   function handleClick() {
+//     count += 1;
+//     console.log(count);
+//   }
+
+//   return (
+//     <div className="container">
+//       <div className="count">{count}</div>
+//       <img src={props.imageUrl} onClick={handleClick}/>
+//       <h1>{props.title}</h1>
+//       <p>{props.subtitle}</p>
+//     </div>
+//   );
+// }
+
+var Hero = React.createClass({
+  getInitialState: function() { //задаем начальное состояние
+    return {
+      count: 0
+    };
+  },
+
+  handleClick: function() {
+    this.setState({count: this.state.count + 1}); //меняем состояние
+  },
+
+  render: function() {
+    return (
+      <div className="container">
+        <div className="count">{this.state.count}</div>
+        <img src={this.props.imageUrl} onClick={this.handleClick}/>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.subtitle}</p>
+      </div>
+    );
+  }
+});
 
 ReactDOM.render(
   <div>
