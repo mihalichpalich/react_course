c3fb6f01028f3fd9ef5ac650ac4d243d1a8d3b6a
mihalichpalich <pavlovtfspb@gmail.com>
2018-01-31 11:56:06 +0300

    познакомился с виртуальным dom

--- app.jsx
+++ app.jsx
@@ -1,8 +1,22 @@
+// var jsx =
+// <div className="container">
+//   <img src="https://cdn.worldvectorlogo.com/logos/react.svg" />
+//   <h1>React</h1>
+//   <p>Библиотека для создания пользовательских интерфейсов</p>
+// </div>;
+
+// ReactDOM.render(jsx, document.getElementById('root'));
+
+// var h1 = React.createElement('h1', null, 'React');
+// var div = React.createElement('div', null, h1);
+
 var jsx =
-<div className="container">
-  <img src="https://cdn.worldvectorlogo.com/logos/react.svg" />
+<div>
   <h1>React</h1>
-  <p>Библиотека для создания пользовательских интерфейсов</p>
 </div>;
 
-ReactDOM.render(jsx, document.getElementById('root'));
+console.log(jsx);
+
+var dom = ReactDOM.render(jsx, document.getElementById('root'));
+
+console.log(dom);
