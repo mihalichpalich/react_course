d14db7c533fb88bddba3c6033f5f6b59ab88eaaa
mihalichpalich <pavlovtfspb@gmail.com>
2018-01-31 11:44:19 +0300

    познакомился с jsx

---
+++ app.jsx
@@ -0,0 +1,8 @@
+var jsx =
+<div className="container">
+  <img src="https://cdn.worldvectorlogo.com/logos/react.svg" />
+  <h1>React</h1>
+  <p>Библиотека для создания пользовательских интерфейсов</p>
+</div>;
+
+ReactDOM.render(jsx, document.getElementById('root'));
