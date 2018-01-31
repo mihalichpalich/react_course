d44afd14aa43d690119b168e6945ef589800f781
mihalichpalich <pavlovtfspb@gmail.com>
2018-01-31 14:55:35 +0300

    создал новую ветку

--- app.js
+++
@@ -1,6 +0,0 @@
-var image = React.createElement('img', {src: 'https://cdn.worldvectorlogo.com/logos/react.svg'});
-var title = React.createElement('h1', null, 'React'); //1-й аргумент - название DOM-элемента, 2-й - свойства, 3-й - содержимое создаваемого элемента
-var subtitle = React.createElement('p', null, 'Библиотека для создания пользовательских интерфейсов');
-var container = React.createElement('div', {className: 'container'}, image, title, subtitle);
-
-ReactDOM.render(container, document.getElementById('root')); //1-й аргумент - название созданного реакт-элемента, 2-й - место, куда его нужно поместить
