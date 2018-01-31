479a5f59f8c7bce8d3beeb01b33ff929e84f097e
mihalichpalich <pavlovtfspb@gmail.com>
2018-01-31 12:07:55 +0300

    изучил компоненты

--- app.jsx
+++ app.jsx
@@ -1,22 +1,11 @@
-// var jsx =
-// <div className="container">
-//   <img src="https://cdn.worldvectorlogo.com/logos/react.svg" />
-//   <h1>React</h1>
-//   <p>Библиотека для создания пользовательских интерфейсов</p>
-// </div>;
+function Hero() {
+  return (
+    <div className="container">
+      <img src="https://cdn.worldvectorlogo.com/logos/react.svg" />
+      <h1>React</h1>
+      <p>Библиотека для создания пользовательских интерфейсов</p>
+    </div>
+  );
+}
 
-// ReactDOM.render(jsx, document.getElementById('root'));
-
-// var h1 = React.createElement('h1', null, 'React');
-// var div = React.createElement('div', null, h1);
-
-var jsx =
-<div>
-  <h1>React</h1>
-</div>;
-
-console.log(jsx);
-
-var dom = ReactDOM.render(jsx, document.getElementById('root'));
-
-console.log(dom);
+ReactDOM.render(<Hero />, document.getElementById('root'));
