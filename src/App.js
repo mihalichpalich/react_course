cc2cb75f3efe4ca70779da065c0958bbfb13f02c
mihalichpalich <pavlovtfspb@gmail.com>
2018-02-03 20:58:21 +0300

    познакомился с create-react-app

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() { //самый важный метод класса
+    return <div>Hello world</div>; // return всегда обязателен, он возвращает отрендеренные элементы
+  }
+}
+
+export default App;
