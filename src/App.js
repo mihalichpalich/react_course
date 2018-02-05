f826d2e29dffa7a4ad18ad7fe6e5d5335b5313f4
mihalichpalich <pavlovtfspb@gmail.com>
2018-02-05 15:28:02 +0300

    освоил работу css в react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 
+import './App.css';
+
 import RegistrationForm from './RegistrationForm.js';
 
 class App extends Component {
-
-
   render() {
     return (
-      <div>
+      <div className="container">
         <RegistrationForm />
       </div>
     );
--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+import './RegistrationForm.css';
+
 class RegistrationForm extends Component {
   constructor(props) {
     super(props);
@@ -26,8 +28,9 @@ class RegistrationForm extends Component {
           placeholder="E-mail"
           value={this.state.email}
           onChange={this.handleEmailChange}
+          className="emailField"
         />
-        <button>Save</button>
+        <button className="submitBtn">Save</button>
       </form>
     );
   }
