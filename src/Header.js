569e70d3cb7eec7e3db40f387ebaf9c75d337531
mihalichpalich <pavlovtfspb@gmail.com>
2018-02-05 12:49:14 +0300

    изучил валидацию react props с помощью PropTypes

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
+  static propTypes = {
+    items: PropTypes.array.isRequired, //isRequired показывает, что эти данные нужно передавать обязательно
+    isLoading: PropTypes.bool, //bool может быть не обязательно
+    submit: PropTypes.func.isRequired,
+    title: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(['news', 'photos']), //хотим получить на вход тип, который будет равняться строке 'news' или 'photos'
+    user: PropTypes.shape({ //свойство должно быть объектом определенной формы (указываются имена свойств объекта и их типы)
+      name: PropTypes.string,
+      age: PropTypes.number
+    }),
+    users: PropTypes.arrayOf( //свойство должно быть массивом значений определенного типа
+      PropTypes.shape({
+        name: PropTypes.string,
+        age: PropTypes.number
+      })
+    )
+  };
+
   render() {
     console.log('items', this.props.items); // берем переданный menu, из атрибута items, который находится в props
 
