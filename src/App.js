5544733e72e7ba3b461f426645d234920ed17b86
mihalichpalich <pavlovtfspb@gmail.com>
2018-02-05 19:49:05 +0300

    освоил refs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 
-import './App.css';
-
-import RegistrationForm from './RegistrationForm.js';
-
 class App extends Component {
+  submit = () => {
+    console.log('submit', this.testInput.value);
+  }
+
   render() {
     return (
-      <div className="container">
-        <RegistrationForm />
+      <div>
+        <input type="text" placeholder="test" ref={(input) => this.testInput = input}/>{/* ref лучше использовать когда нельзя использовать props или state*/}
+        <button onClick={this.submit}>Submit</button>
       </div>
     );
   }
